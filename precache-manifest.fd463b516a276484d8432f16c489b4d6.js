919fcbb6f66f75f64fdbcbd39784da7abab9a36f
Zayac11 <brunogrbstudios@gmail.com>
2022-01-30 16:59:17 +0300

    Updates

--- precache-manifest.fd463b516a276484d8432f16c489b4d6.js
+++ precache-manifest.fd463b516a276484d8432f16c489b4d6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e513dcfe636adccfcd7fe48e351a6a21",
+    "revision": "82c3189df9b21e1e4e28af129b8d4c18",
     "url": "/pwa_update/index.html"
   },
   {
-    "revision": "115a42f3a46ea0b88f9e",
-    "url": "/pwa_update/static/css/main.07c8d3fc.chunk.css"
+    "revision": "6a1a374a0fd7eefdf24a",
+    "url": "/pwa_update/static/css/main.f1f837a1.chunk.css"
   },
   {
     "revision": "a076fda68393d00ed1d6",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pwa_update/static/js/3.222befab.chunk.js"
   },
   {
-    "revision": "115a42f3a46ea0b88f9e",
-    "url": "/pwa_update/static/js/main.ba1e6183.chunk.js"
+    "revision": "6a1a374a0fd7eefdf24a",
+    "url": "/pwa_update/static/js/main.f1fcfb1f.chunk.js"
   },
   {
     "revision": "78109bbf7fa10e2e9269",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwa_update/precache-manifest.870091f3a1ee4b2802a246b1f41dc9e8.js"
+  "/pwa_update/precache-manifest.fd463b516a276484d8432f16c489b4d6.js"
 );
 
 self.addEventListener('message', (event) => {
