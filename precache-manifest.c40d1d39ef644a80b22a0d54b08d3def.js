ef7dcf041a33afaa892ff15b077a8cfa755ae1d2
Zayac11 <brunogrbstudios@gmail.com>
2022-01-30 16:03:55 +0300

    Updates

---
+++ precache-manifest.c40d1d39ef644a80b22a0d54b08d3def.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e394f5944c87e5e85faeeb257d7cf350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cff39a510ae18da3086",
+    "url": "/static/css/main.556909e5.chunk.css"
+  },
+  {
+    "revision": "a076fda68393d00ed1d6",
+    "url": "/static/js/2.46faff16.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.46faff16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76200fd68f1713d58a70",
+    "url": "/static/js/3.222befab.chunk.js"
+  },
+  {
+    "revision": "5cff39a510ae18da3086",
+    "url": "/static/js/main.855221a8.chunk.js"
+  },
+  {
+    "revision": "46a5929a91447c670052",
+    "url": "/static/js/runtime-main.0e3ea446.js"
+  }
+]);
