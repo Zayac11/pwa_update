5dc3b1a528c3e1cf94892ae89b006ee1f13f633c
Zayac11 <brunogrbstudios@gmail.com>
2022-01-30 19:22:13 +0300

    text text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import './App.css'
-import logo from './assets/logo.png'
 import Notify from './components/Notify/Notify'
 
 function App() {
     return (
         <div className='container'>
             text text text update update
-            <div>
-                text v div
-            </div>
-            <div>
-                final exam chtobi ponyat' nakonetc
-            </div>
             <button id={'notifications'}>hello</button>
             <Notify />
         </div>
